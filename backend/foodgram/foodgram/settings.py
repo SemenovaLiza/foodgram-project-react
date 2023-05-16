82f0f9dcdc7bdc272f7d38153d1b428047bce773
Семенова Елизавета <thealeeeaaa23@gmail.com>
2023-05-16 15:32:09 +0400

    deploy

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -152,6 +152,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 
