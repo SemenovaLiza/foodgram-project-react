22e7044634e8e0903776f4c94df6cf0364666f5a
Семенова Елизавета <thealeeeaaa23@gmail.com>
2023-07-26 18:45:32 +0400

    fixed flake8

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import (Favorite, Ingredient, Recipe, RecipesIngredient,
-                     ShoppingCart, Tag, RecipesTag)
+                     RecipesTag, ShoppingCart, Tag)
 
 
 class TagsInLine(admin.TabularInline):
